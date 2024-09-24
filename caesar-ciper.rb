dd8eb85111635a97460bdd368813b7c8c34928f4
Sadaf Al Arabi <35129264+Sadaaf@users.noreply.github.com>
2024-09-24 09:27:44 -0400

    created caesar-cipher

---
+++ caesar-ciper.rb
@@ -0,0 +1,28 @@
+def caesar_cypher(message, shift_factor)
+  updated_asciis = Array.new
+  encrypted_message = ""
+  message.bytes.each do |char|
+    if char >=65 && char <=90
+      char+= shift_factor
+      loop do
+        break if char <= 90
+        char=64+(char-90)
+      end
+    elsif char >=97 && char <=122
+      char+= shift_factor
+      loop do
+        break if char <= 122
+        char=96+(char-122)
+      end
+    end
+    updated_asciis << char
+  end
+  updated_asciis.each do |ascii|
+    encrypted_message.concat(ascii.chr)
+  end
+  encrypted_message
+end
+
+message = "Sadaf that was awesome!"
+
+p caesar_cypher(message, 20)
